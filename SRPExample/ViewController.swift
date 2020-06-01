1125bd2f94d581e5c38a282e928f8b0f5cd7cd81
Inna Lapteva <ilapteva@snopico.com>
2020-06-01 09:19:54 +0500

    Initial Commit

---
+++ SRPExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SRPExample
+//
+//  Created by Инна Лаптева on 01.06.2020.
+//  Copyright © 2020 Инна Лаптева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
